c95f605c39483dd0816712c6cb9c44f3e7f3a60f
MAKUL-eg <makul@eg.dk>
2024-07-10 10:49:06 +0530

    menu creaed

--- LoanCalcuationUnitTest/Program.cs
+++ LoanCalcuationUnitTest/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace LoanCalcuationUnitTest
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choice;
+            do
+            {
+                Console.WriteLine("----------MENU_____________");
+                Console.WriteLine("1- Calculate annual interest");
+                Console.WriteLine("2- Calculate amount to pay per month");
+                Console.WriteLine("3- Exit");
+                Console.WriteLine("Enter your choice");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:break;
+                    case 2:break;
+                    case 3:return;
+                    default:
+                        Console.WriteLine("Invlaid choice");
+                        break;
+                }
+
+            } while (true);
         }
     }
 }
